eb5e74322ab30bc9c23643714118e1eeb6f58438
Gajendera Kumar <gajenderakumar806@gmail.com>
2024-11-23 22:11:53 +0530

    Longest Substring Without Repeating Characters.

---
+++ String/09_longest_substr_without_repeating_char.js
@@ -0,0 +1,20 @@
+/*
+    Longest Substring Without Repeating Characters.     Leetcode Problem
+    <--------------------------------------------->     <--------------->
+
+    Input: s = "abcabcbb",  Output: 3
+
+    Explanation: The answer is "abc", with the length of 3.
+
+
+    Input: s = "abcabcbb",  Output: 3
+    
+    Explanation: The answer is "abc", with the length of 3.
+
+
+    Input: s = "pwwkew",  Output: 3
+
+    Explanation: The answer is "wke", with the length of 3.
+    Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+*/
+
